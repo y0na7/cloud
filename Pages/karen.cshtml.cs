46597ab427ad1d53f7d45c5a09d700a955858763
Jonas <jhraun@gmail.com>
2022-10-06 10:42:39 +0200

    karen

---
+++ Pages/karen.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IBAS_menu.Pages
+{
+    public class karenModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
